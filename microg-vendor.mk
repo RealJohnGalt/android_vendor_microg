e8e6002db9f1eed8d9b9f85dbd18ea165df3ea1d
John Galt <johngaltfirstrun@gmail.com>
2020-10-16 10:08:29 -0400

    initial microg vendor repo

---
+++ microg-vendor.mk
@@ -0,0 +1,15 @@
+# Some decent microg defaults
+PRODUCT_PACKAGES += \
+    GmsCore \
+    MicroGGSFProxy \
+    FakeStore \
+    AuroraStore \
+    AuroraDroid \
+    AuroraServices \
+    GoogleMapsFramework \
+    AppleNLPBackend \
+    DejaVuLocationService \
+    LocalGSMNLPBackend \
+    LocalWiFiNLPBackend \
+    NominatimNlpBackend \
+    MozillaNlpBackend
